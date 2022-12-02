9849dcdd8930af0c95859455e75cecedc7a05a68
Lauren Kimpel <lekimpel@gmail.com>
2022-12-02 14:35:24 -0800

    clean up some of the graph construction

--- standalone_examples/expandercode_basic.py
+++ standalone_examples/expandercode_basic.py
@@ -1,37 +1,67 @@
 import numpy
 import networkx as nx
+import sympy
 from networkx.algorithms import bipartite
 
 '''
-Generate an unbalanced bipartite (d,c) regular graph with order n
-and return its adjacency matrix
+Generate a random bipartite graph whose edge creation has probability p
+We need to also ensure that this graph is connected!
 '''
-def generate_random_graph(d,c,n):
+def generate_random_graph(n, c, d, p):
     # get a bipartite graph G(W \cup N, E), with |W| = n and |N| = (c/d)*n
-    B = bipartite.complete_bipartite_graph(n, int(float(c)/float(d))*n)
-    W,N = nx.bipartite.sets(B) 
+    B = bipartite.random_graph(n, int(float(c)/float(d))*n, p)
 
-    # make sure it is (d,c)-regular, with c > d
-    # only need to iterate over the first set and remove enough edges so that each vertex is d-regular
-    for i in range(0,len(W)):
-        # if the degree of the vertex is not d, remove edges until it is
-        j = len(W)
-        while(B.degree[i]>d):
-            B.remove_edge(i,j)
-            j+=1
+    while(not nx.is_connected(B)):
+        # regenerate the graph ;)
+        B = bipartite.random_graph(n, int(float(c)/float(d))*n, p)
+        
+    # get the nodes in the bipartite sets of B
+    W,N = nx.bipartite.sets(B)
+    W = list(W)
+    N = list(N)
+    
+    # construct the bipartite adjacency matrix: rows correspond to W, columns to N
+    return [[int(B.has_edge(W[i], N[j])) for j in range(len(N))] for i in range(len(W))]
 
-    for i in range(len(W), len(W)+len(N)):
-        j = 0
-        while(B.degree[i]>c):
-            B.remove_edge(j,i)
-            j += 1
-    return nx.adjacency_matrix(B)
+'''
+Calculate the cheeger constant of a particular graph
+'''
+def get_cheeger_constant(B):
+    return 
 
-def encode():
+'''
+Calculate the rate for a particular linear code
+'''
+def calc_rate(B):
+    return
+'''
+Calculate the distance for a particular linear code
+'''
+def calc_distance(B):
+    return 
+
+'''
+Output the graphical model of B
+'''
+def visualize(B):
+    return 
+
+'''
+Get the code by computing the generator matrix
+'''
+def get_generator_matrix(A):
+    return 
+
+'''
+Encode data using a linear code whose parity-check matrix is the adjacency matrix
+of a biregular (c,d) graph. 
+'''
+def encode(data):
     return
 
 def decode():
     return 
 
 
-print(generate_random_graph(1,2,3))
+A = generate_random_graph(3,7,3,0.5)
+print(A)
